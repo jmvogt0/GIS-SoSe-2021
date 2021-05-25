97fecfb4fede365ba0ec3c5b66a94810e57e8e0d
Jan <jan.michael.vogt@hs-furtwangen.de>
2021-05-25 23:48:54 +0200

    finished first step of task 3.1

--- Aufgaben/Aufgabe3.1/script.js
+++ Aufgaben/Aufgabe3.1/script.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log("HelloWorld");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.P_3_1Server = void 0;
+const Http = require("http");
+//import * as Url from "url";
+var P_3_1Server;
+(function (P_3_1Server) {
+    //Ausgabe Starting Server
+    console.log("Starting server");
+    //Port nummer definieren
+    let port = Number(process.env.PORT);
+    //Falls kein Port definiert ist, wird Port 8100 definiert
+    if (!port)
+        port = 8100;
+    //Server erstellen
+    let server = Http.createServer();
+    //der Variable Server einen Eventlistener request hinzufügen, welcher die Funktion handleRequest aufruft
+    server.addListener("request", handleRequest);
+    //der Variable Server einen Eventlistener request hinzufügen, welcher die Funktion handleRequest aufruft
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    //Ausgabe in der Konsole: "Listening"
+    function handleListen() {
+        console.log("Listening");
+    }
+    //Funktion handleRequest mit den geforderten Parametern _request vom Typ Http.IncomingMessage und _response vom Typ Http.ServerResponse
+    //Der Rückgabewert der Funktion ist void
+    function handleRequest(_request, _response) {
+        //Ausgabe in der Konsole "I hear voices". sobald die Funktion aufgerufen wird
+        console.log("I hear voices!");
+        //
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        //
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Funktion write aufrufen, welchen den Inhalt der Url dann in das html schreibt
+        _response.write(_request.url);
+        console.log(_request.url);
+        _response.end();
+    }
+})(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=script.js.map
--- Aufgaben/Aufgabe3.1/script.ts
+++ Aufgaben/Aufgabe3.1/script.ts
@@ -1,1 +1,40 @@
-console.log("HelloWorld");
+import * as Http from "http";
+//import * as Url from "url";
+
+export namespace P_3_1Server {
+    //Ausgabe Starting Server
+    console.log("Starting server");
+    //Port nummer definieren
+    let port: number = Number(process.env.PORT);
+    //Falls kein Port definiert ist, wird Port 8100 definiert
+    if (!port)
+        port = 8100;
+
+    //Server erstellen
+    let server: Http.Server = Http.createServer();
+    //der Variable Server einen Eventlistener request hinzufügen, welcher die Funktion handleRequest aufruft
+    server.addListener("request", handleRequest);
+    //der Variable Server einen Eventlistener request hinzufügen, welcher die Funktion handleRequest aufruft
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    //Ausgabe in der Konsole: "Listening"
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+    //Funktion handleRequest mit den geforderten Parametern _request vom Typ Http.IncomingMessage und _response vom Typ Http.ServerResponse
+    //Der Rückgabewert der Funktion ist void
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        //Ausgabe in der Konsole "I hear voices". sobald die Funktion aufgerufen wird
+        console.log("I hear voices!");
+        //
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        //
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Funktion write aufrufen, welchen den Inhalt der Url dann in das html schreibt
+        _response.write(_request.url);
+        console.log(_request.url);
+        _response.end();
+    }
+}
