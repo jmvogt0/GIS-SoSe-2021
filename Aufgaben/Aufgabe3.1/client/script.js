5c1364103b7ab2ba258f816b4e6fc1ceadb3e218
Jan <jan.michael.vogt@hs-furtwangen.de>
2021-05-27 10:19:10 +0200

    added some comments on script.ts

--- Aufgaben/Aufgabe3.1/client/script.js
+++ Aufgaben/Aufgabe3.1/client/script.js
@@ -1,17 +1,23 @@
 "use strict";
 var Aufgabe3_1;
 (function (Aufgabe3_1) {
+    //Synchrone Funktion SendData, welche die URL erwitert
     function sendData() {
+        //Daten asus Formular auslesen
         let formData = new FormData(document.forms[0]);
         let url = "https://gis-sose-2021-jmvogt.herokuapp.com/";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
+        //query an die Url anhängen
         url = url + "?" + query.toString();
+        //asynchrone Funktion communicate aufrufen, welche die Anfrage per fetch an den server schickt
         communicate(url);
     }
     document.querySelector("#sendData").addEventListener("click", sendData);
     async function communicate(_url) {
+        //Anfrage an den Server
         let response = await fetch(_url);
+        //Antwort vom server
         let responseString = await response.text();
         console.log(responseString);
     }
--- Aufgaben/Aufgabe3.1/client/script.ts
+++ Aufgaben/Aufgabe3.1/client/script.ts
@@ -1,19 +1,23 @@
 namespace Aufgabe3_1 {
+    //Synchrone Funktion SendData, welche die URL erwitert
     function sendData (): void {
+        //Daten asus Formular auslesen
         let formData: FormData = new FormData(document.forms[0]);
         let url: RequestInfo = "https://gis-sose-2021-jmvogt.herokuapp.com/";
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
+        //query an die Url anhängen
         url = url + "?" + query.toString();
-
+        //asynchrone Funktion communicate aufrufen, welche die Anfrage per fetch an den server schickt
         communicate (url);
     }
     document.querySelector("#sendData").addEventListener("click", sendData);
 
 
     async function communicate (_url: RequestInfo): Promise<void> {
-
+        //Anfrage an den Server
         let response: Response = await fetch(_url);
+        //Antwort vom server
         let responseString: string = await response.text();
         console.log(responseString);
     }
