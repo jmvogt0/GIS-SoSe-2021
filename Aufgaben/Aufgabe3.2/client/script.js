7d628cf099aa405d7fc8cd8f6ea7a1d6182dc0c3
Jan <jan.michael.vogt@hs-furtwangen.de>
2021-06-02 10:25:07 +0200

    finished task 3.2

---
+++ Aufgaben/Aufgabe3.2/client/script.js
@@ -0,0 +1,39 @@
+"use strict";
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    //Synchrone Funktion SendData, welche die URL erweitert
+    function sendDataHtml() {
+        let url = "https://gis-sose-2021-jmvogt.herokuapp.com/html";
+        let formData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        //query an die Url anhängen
+        url = url + "?" + query.toString();
+        communicateHtml(url);
+    }
+    async function communicateHtml(_url) {
+        let response = await fetch(_url);
+        let responseString = await response.text();
+        //HTML Code während der Laufzeit einfügen
+        let answerOutput = document.getElementById("answer");
+        answerOutput.innerHTML = responseString;
+    }
+    function sendDataJson() {
+        let url = "https://gis-sose-2021-jmvogt.herokuapp.com/json";
+        let formData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        //query an die Url anhängen
+        url = url + "?" + query.toString();
+        communicateJson(url);
+    }
+    async function communicateJson(_url) {
+        let response = await fetch(_url);
+        let responseString = await response.json();
+        console.log(responseString);
+    }
+    //Buttons
+    document.querySelector("#sendDataHtml").addEventListener("click", sendDataHtml);
+    document.querySelector("#sendDataJson").addEventListener("click", sendDataJson);
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgaben/Aufgabe3.2/client/script.ts
@@ -0,0 +1,41 @@
+namespace Aufgabe3_1 {
+
+    //Synchrone Funktion SendData, welche die URL erweitert
+    function sendDataHtml (): void {
+        let url: string = "https://gis-sose-2021-jmvogt.herokuapp.com/html";
+        let formData: FormData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        //query an die Url anhängen
+        url = url + "?" + query.toString();
+        communicateHtml (url);
+    }
+
+    async function communicateHtml (_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url);
+        let responseString: string = await response.text();
+        //HTML Code während der Laufzeit einfügen
+        let answerOutput: HTMLElement = document.getElementById("answer");
+        answerOutput.innerHTML = responseString;
+    }
+
+    function sendDataJson (): void {
+        let url: string = "https://gis-sose-2021-jmvogt.herokuapp.com/json";
+        let formData: FormData = new FormData(document.forms[0]);
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        //query an die Url anhängen
+        url = url + "?" + query.toString();
+        communicateJson (url);
+    }
+ 
+    async function communicateJson (_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url);
+        let responseString: string = await response.json();
+        console.log(responseString);
+    }
+
+    //Buttons
+    document.querySelector("#sendDataHtml").addEventListener("click", sendDataHtml);
+    document.querySelector("#sendDataJson").addEventListener("click", sendDataJson);
+}
